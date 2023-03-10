d136e491f514ce85426bc4d9dc76aeb17e5a2927
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-03-10 01:30:22 -0500

    Create components and its routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { CreateAlbumComponent } from './create-album/create-album.component';
+import { LoginComponent } from './login/login.component';
+import { MyAlbumsComponent } from './my-albums/my-albums.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'my-albums', component: MyAlbumsComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'create-album', component: CreateAlbumComponent},
+  {path:'album-details', component: AlbumDetailsComponent},
+  {path:'upload-picture', component: UploadPictureComponent},
+  {path:'photo-details', component: PhotoDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
